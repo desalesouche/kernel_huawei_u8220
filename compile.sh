387711648eb42a96d71c997c9560c65a89589753
destruct0r <destruct0r@mail.ru>
2012-03-25 15:32:13 +0300

    edit

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cp arch/arm/configs/u8220_defconfig .config
 time make ARCH=arm CROSS_COMPILE=~/build/android-ndk-r6/toolchains/arm-linux-androideabi-4.4.3/prebuilt/linux-x86/bin/arm-linux-androideabi- -j4
