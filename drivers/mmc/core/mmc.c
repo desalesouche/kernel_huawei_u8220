4068feb6bfcd1ecb95d617c98f66a91623545d52
desalesouche <desailly55@hotmail.com>
2013-03-20 12:49:37 +0100

    Revert "mmc:core:put eMMC in sleep (cmd5) mode before suspend"
    This reverts commit 432a8eb8c9b2ec28f6cde06aa701b5a8a9e54b4c.

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -543,14 +543,12 @@ static int mmc_suspend(struct mmc_host *host)
 	BUG_ON(!host->card);
 
 	mmc_claim_host(host);
-	if (mmc_card_can_sleep(host))
-          err = mmc_card_sleep(host);
-        else if (!mmc_host_is_spi(host))
+	if (!mmc_host_is_spi(host))
 		mmc_deselect_cards(host);
 	host->card->state &= ~MMC_STATE_HIGHSPEED;
 	mmc_release_host(host);
 
-	return err;
+	return 0;
 }
 
 /*
