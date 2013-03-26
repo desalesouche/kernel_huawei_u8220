e045a34e0264849a86c42e8a214bc681730b58de
desalesouche <desailly55@hotmail.com>
2013-03-26 20:14:29 +0100

    cou:frequency use an inline function to evaluate
    Use an inline function to evaluate freq_target to avoid duplicate code.
    
    Also, define a macro for the default frequency step.
    
    Signed-off-by: Stratos Karafotis <stratosk@semaphore.gr>

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -31,6 +31,7 @@
 
 #define DEF_FREQUENCY_UP_THRESHOLD		(80)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(20)
+#define DEF_FREQUENCY_STEP      (5) 
 
 /*
  * The polling frequency of this governor depends on the capability of
@@ -95,7 +96,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 5,
+	.freq_step = DEF_FREQUENCY_STEP, 
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
@@ -360,10 +361,21 @@ static struct attribute_group dbs_attr_group = {
 
 /************************** sysfs end ************************/
 
+static inline unsigned int get_freq_target(struct cpufreq_policy *policy)
+{
+        unsigned int freq_target = (dbs_tuners_ins.freq_step * policy->max) /
+                                                                        100;
+
+        /* max freq cannot be less than 100. But who knows... */
+        if (unlikely(freq_target == 0))
+                freq_target = DEF_FREQUENCY_STEP;
+
+        return freq_target;
+}
+
 static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 {
 	unsigned int load = 0;
-	unsigned int freq_target;
 
 	struct cpufreq_policy *policy;
 	unsigned int j;
@@ -437,13 +449,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 		if (this_dbs_info->requested_freq == policy->max)
 			return;
 
-		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
-
-		/* max freq cannot be less than 100. But who knows.... */
-		if (unlikely(freq_target == 0))
-			freq_target = 5;
-
-		this_dbs_info->requested_freq += freq_target;
+                this_dbs_info->requested_freq += get_freq_target(policy); 
 		if (this_dbs_info->requested_freq > policy->max)
 			this_dbs_info->requested_freq = policy->max;
 
@@ -458,9 +464,8 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 	 * policy. To be safe, we focus 10 points under the threshold.
 	 */
 	if (load < (dbs_tuners_ins.down_threshold - 10)) {
-		freq_target = (dbs_tuners_ins.freq_step * policy->max) / 100;
-
-		this_dbs_info->requested_freq -= freq_target;
+		
+                this_dbs_info->requested_freq -= get_freq_target(policy); 
 		if (this_dbs_info->requested_freq < policy->min)
 			this_dbs_info->requested_freq = policy->min;
 
